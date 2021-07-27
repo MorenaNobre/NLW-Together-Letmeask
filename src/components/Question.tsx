ad016b7d6ae1ccba2986c8d7c2bb233899b8ef13
MorenaNobre <morenagnobre@gmail.com>
2021-07-27 11:05:02 -0300

    remove isAnswred and isHighlited icons when question is answered.

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -25,7 +25,7 @@ export function Question({
     <div
       className={cx(
         "question",
-        { aswered: isAnswered },
+        { answered: isAnswered },
         { highlighted: isHighlighted }
       )}
     >
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -80,18 +80,25 @@ export function AdminRoom() {
                 isAnswered={question.isAnswered}
                 isHighlighted={question.isHighlighted}
               >
-                <button
-                  type="button"
-                  onClick={() => handleCheckQuestionAsAnswered(question.id)}
-                >
-                  <img src={checkImg} alt="Marcar pergunta como respondida" />
-                </button>
-                <button
-                  type="button"
-                  onClick={() => handleHighlightQuestion(question.id)}
-                >
-                  <img src={answerImg} alt="Dar destque à pergunta" />
-                </button>
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img
+                        src={checkImg}
+                        alt="Marcar pergunta como respondida"
+                      />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destque à pergunta" />
+                    </button>
+                  </>
+                )}
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
